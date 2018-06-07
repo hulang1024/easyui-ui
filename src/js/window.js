343011c3d0790b74e50c8fbca5dce4a39c9a0329
hulang1024 <1013644379@qq.com>
2018-06-07 19:48:33 +0800

    add more options

--- src/js/window.js
+++ src/js/window.js
@@ -1,6 +1,21 @@
-$.extend($.fn.window.defaults, {
-   shadow: false
-});
-$.extend($.fn.dialog.defaults, {
-   shadow: false
-});
+(function(){
+  var options = {
+     shadow: false,
+     onMinimize: function () {
+         $(this).window('move', {
+             top: "95%"
+         }).window('collapse').window('open');
+     },
+     onMaximize: function () {
+         if ($(this).panel('options').collapsed) {
+             $(this).window('expand');
+         }
+     },
+     onRestore: function() {
+         $(this).window('center');
+     }
+  };
+
+  $.extend($.fn.window.defaults, options);
+  $.extend($.fn.dialog.defaults, options);
+})();
